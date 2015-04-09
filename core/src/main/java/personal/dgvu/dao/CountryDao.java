2f353331a8d81ab6ca7b28e0fc64937162db0eec
duongvu89 <duongvu89@gmail.com>
2015-04-09 23:16:24 +0700

    One-to-one relationship

---
+++ core/src/main/java/personal/dgvu/dao/CountryDao.java
@@ -0,0 +1,10 @@
+package personal.dgvu.dao;
+
+import personal.dgvu.model.Country;
+
+/**
+ * Created by Duong Vu on 09-Apr-15.
+ */
+public interface CountryDao extends GenericDao<Country, String> {
+
+}
